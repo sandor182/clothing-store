4aef80be3748f27eafd5765e7371f77888eedb26
Csaszar Sandor <csaszar@mebedo.eu>
2020-03-19 12:08:59 +0200

    adding sign-in component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,7 @@ import './header.styles.scss';
 const Header = () => (
   <div className='header'>
     <Link className='logo-container' to='/'>
-      <Logo className='logo' />>
+      <Logo className='logo' />
     </Link>
     <div className='options'>
       <Link className='option' to='/shop'>
@@ -15,6 +15,9 @@ const Header = () => (
       </Link>
       <Link className='option' to='/shop'>
         CONTACT
+      </Link>
+      <Link className='option' to='/signIn'>
+        SIGN IN
       </Link>
     </div>
   </div>
