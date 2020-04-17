762de8e56616885ef86bff6df468076e9dc37298
Csaszar Sandor <csaszar@mebedo.eu>
2020-04-17 14:18:22 +0300

    updating stripe intgration and uptading our favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import {
   selectCartItems,
@@ -33,9 +34,13 @@ const CheckoutPage = ({ cartItems, total }) => (
     {cartItems.map(cartItem => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-    <div className='total'>
-      <span>Total: ${total}</span>
+    <div className='total'>Total: ${total}</div>
+    <div className='test-warning'>
+      Please use the following test credit card for paments*
+      <br />
+      4242 4242 4242 4242 - Exp: 12/20 - CVC:123
     </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
