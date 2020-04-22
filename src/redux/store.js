e1613cfdb27635342a36ec420996d6925559fdd8
Csaszar Sandor <csaszar@mebedo.eu>
2020-04-22 17:43:11 +0300

    fixed process error

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,7 @@ import rootReducer from './root-reducer';
 
 const middlewares = [];
 
-if (proccess.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
